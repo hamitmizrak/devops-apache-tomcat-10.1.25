9424ab0dd8312c8548b787746b79e3478f65fba1
Hamit Mizrak <hamitmizrak@gmail.com>
2024-06-26 22:22:21 +0300

    Dockerfile

--- tomcat_tutorials/deploy1.sh
+++ tomcat_tutorials/deploy1.sh
@@ -5,6 +5,7 @@ docker-compose -f _3_docker-compose.yml up -d
 
 # Container Image Bağlan
 winpty docker container  exec -it  my_tomcat2 bash
+cat /usr/local/tomcat/conf/server.xml
 
 # Başka bir container üzeriden volume bağlanmak
 # winpty docker run -it --rm --name volume_container \
